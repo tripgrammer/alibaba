d1d25df86ea233fda9f191c6556b5ae897d8f4be
Amirmohammad mehri <amirmohammadmehri@nobody.invalid>
2020-07-20 16:18:06 +0430

    add conceptual modeling for hotels

--- alibaba/settings.py
+++ alibaba/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'hotel',
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'alibaba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,4 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+#CKEditor Settings
+#Static Files
+
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+
+#CKEditor
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extension = ['.jpg', '.png']
+    if not ext.lower() in valid_extension:
+        raise ValidationError('Unsupported file extension.')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extension])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/hotel_cover/', null=False, blank=False, validators=[validate_file_extension])
+    star = models.IntegerField(default=0, blank=False)
+    city = models.CharField(max_length=128, null=False, blank=False)
+    # content = RichTextField()
+    # created_at = models.DateTimeField(default=datetime.now, blank=False)
+    def __str__(self):
+        return self.name
+

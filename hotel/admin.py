120de3287a10a641c31b21a726b0dd778175364a
Amirmohammad mehri <amirmohammadmehri@nobody.invalid>
2020-07-20 17:33:14 +0430

    add requirements.txt and custom admin model

--- hotel/admin.py
+++ hotel/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Hotel)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class HotelAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'city']
+    list_display = ['name', 'star', 'city']
+
+
+admin.site.register(Hotel, HotelAdmin)
+
--- hotel/models.py
+++ hotel/models.py
@@ -26,6 +26,4 @@ class Hotel(models.Model):
     city = models.CharField(max_length=128, null=False, blank=False)
     # content = RichTextField()
     # created_at = models.DateTimeField(default=datetime.now, blank=False)
-    def __str__(self):
-        return self.name
 
